dbac1e1e95d349c4279dfea782ec0aa894b3ad19
j3romey <jerome.adviento@hotmail.com>
2018-02-01 18:53:00 -0700

    something to push

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+	var divs = document.querySelectorAll("div");
+
+	$('#header').load('testing-python/components/navbar.html')
+
+
+	/*for(var i = 0; i < divs.length; i++){
+		divs[i].style.backgroundImage = "url(testing-python/images" + (i+1) +".png)"
+	}*/
+
+});
+
+/*
+
+$(document).ready(function(){
+	$('#header').load('/components/navbar.html')
+});
+
+*/
